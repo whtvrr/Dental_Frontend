7eba6fa0ca6671b31c9259fb03269a3f303f79ff
whtvrr <samalalib@gmail.com>
2025-07-19 15:43:25 +0600

    feat: BASE, styles, setup, datagrid, sidebar, themeModes, etc.

---
+++ dental/src/data/mockData.js
@@ -0,0 +1,32 @@
+export const mockDataTeam = [
+    {
+        id:1,
+        name: "Samal Ali",
+        email: "samalalib@gmail.com",
+        age: 21,
+        phone: "87013054512",
+        access: "admin",
+    },
+
+    {
+        id:2,
+        name: "Zhangeldy Beisenov",
+        email: "zhantik@gmail.com",
+        age: 22,
+        phone: "870000000",
+        access: "dentist",
+    }
+] 
+
+export const mockDataContacts = [
+    {
+        id:1,
+        name: "Samal Ali",
+        email: "samalalib@gmail.com",
+        age: 21,
+        phone: "87013054512",
+        address: "bla street",
+        city: "Aktobe"
+
+    }
+] 
---
+++ dental/src/scenes/dashboard/index.jsx
@@ -0,0 +1,38 @@
+import { Box, useTheme } from "@mui/material"; //Button, IconButton, Typography,
+import { tokens } from "../../theme";
+//import { mockTransactions } from "../../data/mockData";
+// import DownloadOutlinedIcon from "@mui/icons-material/DownloadOutlined";
+// import EmailIcon from "@mui/icons-material/Email";
+// import PointOfSaleIcon from "@mui/icons-material/PointOfSale";
+// import PersonAddIcon from "@mui/icons-material/PersonAdd";
+import Header from "../../components/Header";
+
+const Dashboard = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+
+  return (
+    <Box m="20px">
+      {/* HEADER */}
+      <Box display="flex" justifyContent="space-between" alignItems="center">
+        <Header title="DASHBOARD" subtitle="Welcome to your dashboard" />
+
+        {/* <Box>
+          <Button
+            sx={{
+              backgroundColor: colors.blueAccent[700],
+              color: colors.grey[100],
+              fontSize: "14px",
+              fontWeight: "bold",
+              padding: "10px 20px",
+            }}
+          >
+            <DownloadOutlinedIcon sx={{ mr: "10px" }} />
+            Download Reports
+          </Button>
+        </Box> */}
+      </Box>
+      </Box>
+       );
+      };
+export default Dashboard;
